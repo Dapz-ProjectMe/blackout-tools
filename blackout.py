3cd5fd689029cd567f2eac2c5fad484c6b5be687
Dapz-ProjectMe <budi060722@gmail.com>
2026-08-16 22:00:16 +0700

    Update blackout.py

--- blackout.py
+++ blackout.py
@@ -57,7 +57,8 @@ def main():
             process_center()
 
         elif choice == "7":
-            automation()
+          from modules.project_generator import project_generator
+            project_generator()
 
         elif choice == "8":
             title("SETTINGS")
